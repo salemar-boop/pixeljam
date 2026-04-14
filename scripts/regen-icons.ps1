4b4e625b79d733132108759c7f21edb0f8c72b6f
salemar-boop <salemar@vols.utk.edu>
2026-04-14 13:51:11 -0400

    new icon

--- scripts/regen-icons.ps1
+++ scripts/regen-icons.ps1
@@ -26,22 +26,22 @@ function New-Icon {
   $bmp = New-Object System.Drawing.Bitmap $s, $s
   $g = [System.Drawing.Graphics]::FromImage($bmp)
   $g.SmoothingMode = [System.Drawing.Drawing2D.SmoothingMode]::AntiAlias
-  $g.Clear([System.Drawing.Color]::FromArgb(7, 13, 34))
+  $g.Clear([System.Drawing.Color]::FromArgb(255, 244, 236))
 
   $corner = [float]($s * 0.22)
   $iconPath = New-RRect 0 0 $s $s $corner
   $bgBrush = New-Object System.Drawing.Drawing2D.LinearGradientBrush (
     [System.Drawing.RectangleF]::new(0, 0, $s, $s)
   ), (
-    [System.Drawing.Color]::FromArgb(10, 15, 36)
+    [System.Drawing.Color]::FromArgb(255, 232, 214)
   ), (
-    [System.Drawing.Color]::FromArgb(27, 24, 74)
+    [System.Drawing.Color]::FromArgb(255, 199, 189)
   ), 35
   $g.FillPath($bgBrush, $iconPath)
-  $edgePen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(32, 50, 95), [Math]::Max(2, $s * 0.012))
+  $edgePen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(191, 61, 85), [Math]::Max(2, $s * 0.012))
   $g.DrawPath($edgePen, $iconPath)
 
-  $orb = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(48, 84, 150))
+  $orb = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(255, 216, 195))
   $g.FillEllipse($orb, [int]($s * 0.1), [int]($s * 0.1), [int]($s * 0.8), [int]($s * 0.8))
 
   $jarX = [float]($s * 0.19)
@@ -49,9 +49,9 @@ function New-Icon {
   $jarW = [float]($s * 0.62)
   $jarH = [float]($s * 0.62)
   $strokeW = [float][Math]::Max(2, $s * 0.012)
-  $jarPen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(31, 66, 112), $strokeW)
+  $jarPen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb(139, 30, 52), $strokeW)
 
-  $lidBrush = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(118, 138, 167))
+  $lidBrush = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(227, 232, 242))
   $lidPath = New-RRect ($jarX + $jarW * 0.14) ($jarY - $jarH * 0.15) ($jarW * 0.72) ($jarH * 0.16) ($jarW * 0.05)
   $g.FillPath($lidBrush, $lidPath)
   $g.DrawPath($jarPen, $lidPath)
@@ -62,14 +62,14 @@ function New-Icon {
     $t = $i / [Math]::Max(1, $steps - 1)
     if ($t -lt 0.45) {
       $tt = $t / 0.45
-      $r = [int](34 + (167 - 34) * $tt)
-      $gr = [int](211 + (139 - 211) * $tt)
-      $bl = [int](238 + (250 - 238) * $tt)
+      $r = [int](255 + (234 - 255) * $tt)
+      $gr = [int](120 + (79 - 120) * $tt)
+      $bl = [int](126 + (97 - 126) * $tt)
     } else {
       $tt = ($t - 0.45) / 0.55
-      $r = [int](167 + (124 - 167) * $tt)
-      $gr = [int](139 + (58 - 139) * $tt)
-      $bl = [int](250 + (237 - 250) * $tt)
+      $r = [int](234 + (183 - 234) * $tt)
+      $gr = [int](79 + (31 - 79) * $tt)
+      $bl = [int](97 + (61 - 97) * $tt)
     }
     $linePen = New-Object System.Drawing.Pen ([System.Drawing.Color]::FromArgb($r, $gr, $bl))
     $yy = $jarY + $i
@@ -82,14 +82,14 @@ function New-Icon {
   $shine = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(65, 255, 255, 255))
   $g.FillPath($shine, $shinePath)
 
-  $star1 = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(236, 255, 243, 199))
+  $star1 = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(238, 255, 234, 172))
   $ps = [float][Math]::Max(2, $s * 0.018)
   $sx = $jarX + $jarW * 0.73
   $sy = $jarY + $jarH * 0.34
   $g.FillRectangle($star1, $sx, $sy - 2 * $ps, $ps, 4 * $ps)
   $g.FillRectangle($star1, $sx - 2 * $ps, $sy, 5 * $ps, $ps)
 
-  $star2 = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(182, 165, 243, 252))
+  $star2 = New-Object System.Drawing.SolidBrush ([System.Drawing.Color]::FromArgb(178, 255, 200, 182))
   $sx2 = $jarX + $jarW * 0.28
   $sy2 = $jarY + $jarH * 0.72
   $g.FillRectangle($star2, $sx2, $sy2 - 2 * $ps, $ps, 4 * $ps)
